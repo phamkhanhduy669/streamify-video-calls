54c034f21b3a23f445e6e780b78cbee3ed9e46a7
phamkhanhduy669 <phamkhanhduy669@gmail.com>
2025-10-17 22:44:28 +0700

    fix image

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getProfile, updateProfile } from "../lib/api";
 import PageLoader from "../components/PageLoader";
 import { LANGUAGES, LANGUAGE_TO_FLAG } from "../constants";
-import { ShuffleIcon, LoaderIcon, MapPinIcon } from "lucide-react";
+import { ShuffleIcon, MapPinIcon } from "lucide-react";
 import { getLanguageFlag } from "../components/FriendCard";
 import { capitialize } from "../lib/utils";
 import toast from "react-hot-toast";
@@ -41,12 +41,11 @@ const ProfilePage = () => {
       learningLanguage: user.learningLanguage || "",
       location: user.location || "",
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   if (isLoading) return <PageLoader />;
 
-  const user = data?.user;
+  
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -81,17 +80,15 @@ const ProfilePage = () => {
           <div className="card p-4">
             <div className="flex flex-col items-center gap-4">
               <div className="size-32 rounded-full overflow-hidden bg-base-300">
-                {form.profilePic ? (
-                  <img src={form.profilePic} alt="Profile Preview" className="w-full h-full object-cover" />
-                ) : (
-                  <div className="flex items-center justify-center h-full">
-                    <svg className="w-16 h-16 opacity-40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12 12a5 5 0 100-10 5 5 0 000 10z" stroke="currentColor" strokeWidth="1.5" />
-                      <path d="M3 21a9 9 0 0118 0" stroke="currentColor" strokeWidth="1.5" />
-                    </svg>
-                  </div>
-                )}
-
+                <img
+                  src={form.profilePic || "/i.png"}
+                  alt="Profile Preview"
+                  className="w-full h-full object-cover"
+                  onError={(e) => {
+                    if (e.currentTarget.src.endsWith("/i.png")) return; // stop infinite loop
+                    e.currentTarget.src = "/i.png";
+                  }}
+                />
               </div>
 
               <div className="flex items-center gap-2">
