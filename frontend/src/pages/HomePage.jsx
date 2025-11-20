8e14107678beab275fe54475af056570cb8fab40
nguyenmanhdung23020520 <23020520@vnu.edu.vn>
2025-11-20 23:18:09 +0700

    update search

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import {
   getOutgoingFriendReqs,
   getRecommendedUsers,
-  getUserFriends,
   sendFriendRequest,
   searchUsers,
 } from "../lib/api";
