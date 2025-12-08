f88944622fab7f3cc430722da73a604c4039db3e
phamkhanhduy669 <phamkhanhduy669@gmail.com>
2025-12-08 16:03:08 +0700

    Add chatbot configuration script and update index.html for chatbot integration

---
+++ frontend/dify-config.js
@@ -0,0 +1,17 @@
+window.difyChatbotConfig = {
+  token: '9tkDY1Iz8Oz9S0Vi',
+  inputs: {
+    // You can define the inputs from the Start node here
+    // key is the variable name
+    // e.g.
+    // name: "NAME"
+  },
+  systemVariables: {
+    // user_id: 'YOU CAN DEFINE USER ID HERE',
+    // conversation_id: 'YOU CAN DEFINE CONVERSATION ID HERE, IT MUST BE A VALID UUID',
+  },
+  userVariables: {
+    // avatar_url: 'YOU CAN DEFINE USER AVATAR URL HERE',
+    // name: 'YOU CAN DEFINE USER NAME HERE',
+  },
+}
